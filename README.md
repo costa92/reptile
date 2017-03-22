dfdaeb2a2fb749781478e63f2b8be14f263b6d6e
costa92 <944310536@qq.com>
2017-03-22 15:48:01 +0800

    update md

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # reptile
+
+get.py 是爬 http://zerospace.asika.tw/ 网站的图片
+
+text.py 是爬 http://www.win4000.com 网站的图片，还没有写完正
+
+
+    
+
+
---
+++ dome_win.py
@@ -0,0 +1,63 @@
+import urllib.request  # 网络访问模块
+import random  # 随机数生成模块
+import re  # 正则表达式模块
+import os  # 目录结构处理模块
+import sys 
+
+# 初始化配置参数
+number = 10  # 图片收集数量
+path = '/Users/costa92/Documents/img/'  # 图片存放目录
+
+
+# 文件操作
+if not os.path.exists(path):
+	os.makedirs(path)
+	
+
+
+# 图片保存
+def save_img(url, path):
+	message = None
+	try:
+		file = open(path + os.path.basename(url), 'wb')
+		request = urllib.request.urlopen(url)
+		file.write(request.read())
+	except Exception as e:
+		message = str(e)
+	else:
+		message = os.path.basename(url)
+	finally:
+		if not file.closed:
+			file.close()
+		return message
+
+
+# 网络连接
+http = 'http://www.win4000.com/meinv110'  # 目标网址
+position = 290 + int((1000 - number) * random.random())
+ids = range(position, position + number)
+
+for id in ids:
+	try:
+		url = "%s%d.html" % (http, id)  # 后缀生成
+		print(url)
+		request = urllib.request.urlopen(url)
+	except Exception as e:
+		print(e)
+		continue
+	else:
+		buffer = request.read()
+		buffer = buffer.decode('utf8')
+		pattern = r'class="pic-large" src="(.+\.jpg)"'
+		imgurl = re.findall(pattern, buffer)  # 过滤规则
+	
+		if len(imgurl) != 0:
+			pages_pattern =r'</span>\/+([1-9]\d?)'
+			pages = re.findall(pages_pattern, buffer)  # 过滤规则
+			print(pages)
+		
+			print(save_img(imgurl[0],path))
+		else:
+			 continue
+	pass			
+	    
